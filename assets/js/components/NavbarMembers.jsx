76bccd750a1db2c4eac6ef393df0fa3770f9186d
Marina <cormerais10@gmail.com>
2020-04-27 11:22:21 +0200

    modification de la navbar members

--- assets/js/components/NavbarMembers.jsx
+++ assets/js/components/NavbarMembers.jsx
@@ -6,11 +6,11 @@ const NavbarMembers = (props) => {
 
     <nav className="nav nav-tabs  flex-column flex-sm-row mt-3 mb-3">
 
-      <Link to="/members" className="navlink-custom">Membres</Link>
-      <Link to="/teams" className="navlink-custom">Équipes</Link>
-      <a className="navlink-custom" href="#">Coach</a>
-      <a className="navlink-custom" href="#">Dirigeant</a>
-      <a className="navlink-custom" href="#">Statistiques</a>
+      <Link to="/members" className="navlink-custom"><i className="fas fa-user"/>  Membres</Link>
+      <Link to="/teams" className="navlink-custom"><i className="fas fa-users"/>  Équipes</Link>
+      <a className="navlink-custom" href="#"><i className="fas fa-user-tie"/> Coachs</a>
+      <a className="navlink-custom" href="#"> <i className="fas fa-user-tie"/> Dirigeants</a>
+      <a className="navlink-custom" href="#"><i className="fas fa-chart-pie"/> Statistiques</a>
 
     </nav>
 
